ec4a13834a3e315813598c7fa20004340d536122
HY-MBP <hymbp@HY-MBPs-MacBook-Pro.local>
2015-05-24 19:40:53 -0400

    Initial Commit

---
+++ postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  postcard
+//
+//  Created by HY-MBP on 5/24/15.
+//  Copyright (c) 2015 prepai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
